1572b4e4de43034ef0aeaa58f275044f5ebc6709
expert <pprof697@gmail.com>
2024-03-26 12:35:01 +0300

    path-2

--- app/src/main/java/com/example/myapplication/ForgotPasswordActivity.java
+++ app/src/main/java/com/example/myapplication/ForgotPasswordActivity.java
@@ -11,27 +11,24 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class ForgotPasswordActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.sign_up);
+        setContentView(R.layout.forgot_password);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
-
         });
-//        ImageButton redi = findViewById(R.id.imageView);
-//        redi.setOnClickListener(new View.OnClickListener() {
+//        ImageButton b = findViewById(R.id.imageButton3);
+//        b.setOnClickListener(new View.OnClickListener() {
 //            @Override
 //            public void onClick(View v) {
-//                Intent intent = new Intent(MainActivity.this, Onboard1Activity.class);
-//                startActivity(intent);
+//                Intent intent = new Intent(ForgotPasswordActivity.this, New)
 //            }
 //        });
     }
-
 }
--- app/src/main/java/com/example/myapplication/SignUpActivity.java
+++ app/src/main/java/com/example/myapplication/SignUpActivity.java
@@ -3,6 +3,8 @@ package com.example.myapplication;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
@@ -11,7 +13,7 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class SignUpActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,16 +24,23 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
-
         });
-//        ImageButton redi = findViewById(R.id.imageView);
-//        redi.setOnClickListener(new View.OnClickListener() {
+
+//        ImageButton check = findViewById(R.id.checkBox);
+//        check.setOnClickListener(new View.OnClickListener() {
 //            @Override
 //            public void onClick(View v) {
-//                Intent intent = new Intent(MainActivity.this, Onboard1Activity.class);
+//                check.setBackgroundResource(R.drawable.selected);
+//            }
+//        });
+//        Button b = findViewById(R.id.button4);
+//        b.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(SignUpActivity.this, LogInActivity.class);
 //                startActivity(intent);
 //            }
 //        });
+        EditText e = findViewById(R.id.edit_email);
     }
-
 }
